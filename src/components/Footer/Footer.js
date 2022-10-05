ed8b3f2858fd7ed24cb06a72cf68011897327280
Tykhon <richard.lee970112@gmail.com>
2022-10-05 14:51:12 +0300

    portfolio

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/VinoU'
+      className='link footer__link'
+    >
+      Created By Vino Uthayakumar
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,77 @@
+const header = {
+    // all the properties are optional - can be left empty or deleted
+    // homepage: 'https://github.com/VinoU',
+    // title: '.',
+};
+
+const about = {
+    // all the properties are optional - can be left empty or deleted
+    name: "Tsung Han",
+    role: "Senior Blockchain Developer",
+    description: "I have more than 10 years of experience in software development using C/C++, C#, Java, Golang, PHP, and Node.js. \n Since 2016, I have been concentrating on Blockchain, Cryptocurrency projects and participated in several Blockchain startups and enterprising companies to build ecosystem on Blockchain. DeFi is my passion and I have developed several DeFi products such as wrapped coin, stablecoin, asset staking, DEX, Lending platform on Ethereum, Tron, Polygon, and Solana. \n I have deep knowledge and strong experience with 0x, AMP, MakerDAO, Uniswap protocols and Solidity, Truffle, Waffle, HardHat. With a passion for finance and trading, I've worked on several trading bots, arbitraging bots on  Bitmex, Binance, Coinbase, Bitfinex, etc. Implemented complex strategies and algorithms using Python, Node.js and integrated them with exchanges using REST API, web socket, etc.   \n Also, I am proficient in building service-oriented architecture and REST API using modern frameworks such as Silex, Symfony, Laravel, Express.js, Koa.js, etc. Actively use Docker, Jenkins, AWS, Docker Cloud, Kubernetes and know all aspects of application building and deployment. Proactive, enthusiastic, optimistic, hard-working, fast-learning are words that describe me the best.",
+    resume: "https://drive.google.com/file/d/1Nh-3P2LQVxgEMViiTSCFbUhhMjglaBss/view?usp=sharing",
+    social: {
+        linkedin: "https://www.linkedin.com/in/vinouthayakumar/",
+        github: "https://github.com/VinoU",
+    },
+};
+
+const projects = [
+    // projects can be added an removed
+    // if there are no projects, Projects section won't show up
+    {
+        name: "Capco",
+        description: "Capco operates at the intersection of business and technology by combining innovative thinking with unrivalled industry knowledge to deliver end-to-end data-driven solutions and fast-track digital initiatives for banking and payments, capital markets, wealth and asset management, insurance, and the energy sector.",
+        stack: ["JAVA", "GIT", "MYSQL"],
+        sourceCode: "https://github.com",
+        livePreview: "https://www.capco.com/",
+    },
+    {
+        name: "CGI",
+        description: "CGI is among the largest IT and business consulting services firms in the world. We are insights-driven and outcome-based to help accelerate returns on your IT and business investments. In all we do, our goal is to build trusted relationships through client proximity, providing industry and technology expertise to help you meet the needs of your customers and citizens. ",
+        stack: ["SASS", "Java", "XML", "Git"],
+        sourceCode: "https://github.com",
+        livePreview: "https://github.com",
+    },
+    {
+        name: "CGI",
+        description: "CGI is among the largest IT and business consulting services firms in the world. We are insights-driven and outcome-based to help accelerate returns on your IT and business investments. In all we do, our goal is to build trusted relationships through client proximity, providing industry and technology expertise to help you meet the needs of your customers and citizens. ",
+        stack: ["javascript", "Adobe experince manager", "Adobe LiveCycle"],
+        sourceCode: "https://github.com",
+        livePreview: "https://github.com",
+    },
+
+    {
+        name: "Ministry government and consumer service",
+        description: "",
+        stack: ["Java", "Adobe LiveCycle", "C#"],
+        sourceCode: "https://github.com",
+        livePreview: "https://github.com",
+    },
+];
+
+const skills = [
+    // skills can be added or removed
+    // if there are no skills, Skills section won't show up
+    "Java",
+    "CSS",
+    "UNIX",
+    "Adobe Experince Manager",
+    "JavaScript",
+    "C#",
+    "MYSQL",
+    "Docker",
+    "SASS",
+    "API/REST web services",
+    "Git",
+    "XML",
+    "SoapUI",
+    "Postman, Docker, Bitbucket",
+];
+
+const contact = {
+    // email is optional - if left empty Contact section won't show up
+    email: "info@vinouthayakumar.com",
+};
+
+export { header, about, projects, skills, contact };
